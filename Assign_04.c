e58bd5cbfd892a66a91bba25a18bdb7149e5e986
u24ai063sunil <u24ai063@aid.svnit.ac.in>
2024-09-12 15:19:30 +0530

    Create Assign_04.c

---
+++ Assign_04.c
@@ -0,0 +1,10 @@
+// C program to calculate gross salary.
+#include <stdio.h>
+int main()
+{
+    int S,GS;
+    printf("enter your salary");
+    scanf("%d",&S);
+    GS=S*12;
+    printf("your gross salary is=%d",GS);
+}
