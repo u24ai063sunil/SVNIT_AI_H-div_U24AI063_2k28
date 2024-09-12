2c2e665413e5c4055517fe0ac3bd6958a7ad5673
u24ai063sunil <u24ai063@aid.svnit.ac.in>
2024-09-12 15:23:27 +0530

    Create Assign_07.c

---
+++ Assign_07.c
@@ -0,0 +1,10 @@
+// C program to calculate area of a triangle.
+#include <stdio.h>
+int main()
+{
+    int B,H,A;
+    printf("enter values of base and height");
+    scanf("%d%d",&B,&H);
+    A=(B*H)/2;
+    printf("Area of triangle is=%d",A);
+}
