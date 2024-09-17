cb08640e8ff8c17e9d314b8c93221f0b49ec6971
u24ai063sunil <u24ai063@aid.svnit.ac.in>
2024-09-17 21:10:56 +0530

    Create Assign_38.c

---
+++ Assign_38.c
@@ -0,0 +1,14 @@
+// C program to print 1,4,9,16,....
+#include <stdio.h>
+int main() 
+{
+    int num=1,n;
+    printf("enter the numbers:\n");
+    scanf("%d",&num);
+    while(num<=n)
+    {
+       printf("%d\n",num*num);
+       num++;
+    }
+    return 0;
+}
