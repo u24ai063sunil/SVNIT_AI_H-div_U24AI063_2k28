12211aa9a384bbd7c3f99d9ed0462e7b1c4e3d41
u24ai063sunil <u24ai063@aid.svnit.ac.in>
2024-09-17 21:00:46 +0530

    Create Assign_27.c

---
+++ Assign_27.c
@@ -0,0 +1,29 @@
+// C program to find whether a number is prime or not.
+#include <stdio.h>
+int main() 
+{
+    int num;
+    printf("enter the number:\n");
+    scanf("%d",&num);
+    if(num>1)
+    {
+    for(int i=2;i<num;i++)
+    {
+        if(num%i==0)
+        {
+            printf("the entered number is not a prime number");
+        }
+        return 1;
+    }
+    printf("the entered number is prime number");
+    }
+    if(num==1)
+    {
+        printf("it is neither prime nor composite");
+    }
+    if(num<1)
+    {
+        printf("invalid input");
+    }
+    return 0;
+}
