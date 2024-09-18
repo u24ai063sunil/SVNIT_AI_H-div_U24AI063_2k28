70678ecfcc20f1b7ec1af5244d12ba400309ec0c
u24ai063sunil <u24ai063@aid.svnit.ac.in>
2024-09-18 12:58:28 +0530

    Create Assign_49.c

---
+++ Assign_49.c
@@ -0,0 +1,14 @@
+// C program to print a pattern of *.
+#include <stdio.h>
+int main() 
+{
+    for(int i=1;i<=5;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+            printf("\n");
+    }
+    return 0;
+}
