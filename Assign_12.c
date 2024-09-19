6344554b33059ef956761fb310ff93310c9d9ae8
u24ai063sunil <u24ai063@aid.svnit.ac.in>
2024-09-19 13:59:02 +0530

    Delete Assign_12.c

--- Assign_12.c
+++
@@ -1,9 +0,0 @@
-// C program to write ASCII code of the entered character.
-#include <stdio.h>
-int main() 
-{
-    char n;
-    printf("enter the character:\n");
-    scanf("%c",&n);
-    printf("ASCII code of n is :%d",n);
-}
