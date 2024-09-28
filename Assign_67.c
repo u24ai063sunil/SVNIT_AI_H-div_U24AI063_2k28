89bcb1ede52ab7b8de796820f42723cf4c9e3332
u24ai063sunil <u24ai063@aid.svnit.ac.in>
2024-09-28 21:27:39 +0530

    Create Assign_67.c

---
+++ Assign_67.c
@@ -0,0 +1,50 @@
+// C program to read two 3*3 matrix and store their difference in the third 3*3 martrix.
+#include <stdio.h>
+int main() 
+{
+    int mtrx1[3][3],mtrx2[3][3],mtrx3[3][3];
+    printf("Enter the elements of the 3*3 matrix 1:\n");
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            scanf("%d",&mtrx1[i][j]);
+        }
+    }
+    printf("Enter the elements of 3*3 matrix 2:\n");
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            scanf("%d",&mtrx2[i][j]);
+        }
+    }
+    printf("3*3 Matrix1:\n");
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            printf("%d\t",mtrx1[i][j]);
+        }
+        printf("\n");
+    }
+    printf("3*3 Matrix2:\n");
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            printf("%d\t",mtrx2[i][j]);
+        }
+        printf("\n");
+    }
+    printf("3*3 Matrix3:\n");  //matrix3=matrix1-matrix2
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            printf("%d\t",mtrx1[i][j]-mtrx2[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
