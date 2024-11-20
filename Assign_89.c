b44c83de86befe557189a22f16707d738c4da3ba
u24ai063sunil <u24ai063@aid.svnit.ac.in>
2024-11-20 18:58:08 +0530

    Create Assign_89.c

---
+++ Assign_89.c
@@ -0,0 +1,25 @@
+// Write a C program to copy elements of one array into another array using pointer.Order of elements in second array must be opposite to first array.
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int N;
+    printf("Enter the size of the arrays:\t");
+    scanf("%d",&N);
+    int arr1[N],arr2[N];
+    int *ptr=arr1;
+    for(int i=0;i<N;i++)
+    {
+         scanf("%d",ptr+i);
+    }     
+    for(int i=0;i<N;i++)
+    {
+         arr2[N-i-1]=*(ptr+i);
+    } 
+    for(int i=0;i<N;i++)
+    {
+         printf("%d",arr2[i]);
+    }      
+    return 0;     
+}
