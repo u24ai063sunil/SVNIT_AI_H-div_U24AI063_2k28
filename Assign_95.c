c666c12b941a88ca4f6b5fc623b5d290aeb7920b
u24ai063sunil <u24ai063@aid.svnit.ac.in>
2024-11-27 12:08:29 +0530

    Create Assign_95.c

---
+++ Assign_95.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+
+struct Employee {
+    int emp_no;
+    char emp_name[50];
+    float basic_pay;
+};
+
+int main() {
+    int n, i;
+
+    printf("Enter the number of employees: ");
+    scanf("%d", &n);
+
+    // Declare an array of 50 Employee structures
+    struct Employee emp[50];
+
+    // Read employee details
+    for (i = 0; i < n; i++) {
+        printf("\nEnter details of employee %d:\n", i + 1);
+        printf("Employee Number: ");
+        scanf("%d", &emp[i].emp_no);
+        printf("Employee Name: ");
+        scanf("%s", emp[i].emp_name);
+        printf("Basic Pay: ");
+        scanf("%f", &emp[i].basic_pay);
+    }
+
+    // Print employee details
+    printf("\nEmployee Details:\n");
+    for (i = 0; i < n; i++) {
+        printf("\nEmployee Number: %d\n", emp[i].emp_no);
+        printf("Employee Name: %s\n", emp[i].emp_name);
+        printf("Basic Pay: %.2f\n", emp[i].basic_pay);
+    }
+
+    return 0;
+}
+
