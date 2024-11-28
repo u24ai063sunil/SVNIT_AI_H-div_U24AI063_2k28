c0a923c4dc9ebe5991266fd8708adedd6a158626
u24ai063sunil <u24ai063@aid.svnit.ac.in>
2024-11-28 14:20:51 +0530

    Create Assign_105.c

---
+++ Assign_105.c
@@ -0,0 +1,38 @@
+/*
+105. WRITE A PROGRAM IN C TO PRINT ALL PERMUTATIONS OF A GIVEN STRING USING POINTERS.
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+void swap(char* a, char* b) {
+    char temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void permute(char* str, int l, int r) {
+    if (l == r) {
+        printf("%s\n", str);
+    } else {
+        for (int i = l; i <= r; i++) {
+            swap((str + l), (str + i));
+            permute(str, l + 1, r);
+            swap((str + l), (str + i)); // Backtrack
+        }
+    }
+}
+
+int main() {
+    char str[100];
+    
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    
+    str[strcspn(str, "\n")] = '\0'; // Remove the newline character from input
+
+    int n = strlen(str);
+    permute(str, 0, n - 1);
+    
+    return 0;
+}
